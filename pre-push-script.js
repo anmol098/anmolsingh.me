89bc96ff51689c3feb232bf0482621081d0e2676
Anmol <anmolpratapsingh1997@gmail.com>
2020-08-26 17:25:04 +0530

    Commit from terminal

---
+++ pre-push-script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const replace = require('replace-in-file');
+var moment = require('moment'); // require
+var d = moment().format('DD-MMM-YYYY');
+
+const options = {
+  files: './src/config.js',
+  from: /[0-9]{2}-[A-Za-z]{3}-[0-9]{4}/g,
+  to: `${d}`,
+};
+try {
+  const results = replace.sync(options);
+  console.log('Replacement results:', results);
+} catch (error) {
+  console.error('Error occurred:', error);
+  process.exit(1);
+}
