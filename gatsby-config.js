498c53024d47f0f7c45ee61b7216a927f9053b43
Anmol <anmolpratapsingh1997@gmail.com>
2020-11-22 01:06:38 +0530

    FEAT: TRIED ADDING APIS FOR SERVER-LESS

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,16 +60,6 @@ module.exports = {
             },
           },
           {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-images
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 700,
-              linkImagesToOriginal: true,
-              quality: 90,
-              tracedSVG: { color: config.colors.green },
-            },
-          },
-          {
             // https://www.gatsbyjs.org/packages/gatsby-remark-code-titles/
             resolve: 'gatsby-remark-code-titles',
           }, // IMPORTANT: this must be ahead of other plugins that use code blocks
